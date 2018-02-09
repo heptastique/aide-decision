8d1678e2619114afd5420ff949ee41f4eadfe3a7
Victor <vb4007@gmail.com>
2018-02-09 10:41:36 +0100

    Responsable stock
    Co-Authored-By: Vincent R <amostrask@users.noreply.github.com>
    Co-Authored-By: Lucas Ono <lulucasono@users.noreply.github.com>
    Co-Authored-By: m4p4ch3 <etienne.raquin@insa-lyon.fr>
    Co-Authored-By: Juliette IMBERT <mordaina@users.noreply.github.com>
    Co-Authored-By: osubou <osubou@users.noreply.github.com>
    Co-Authored-By: rathkori <rathkori@users.noreply.github.com>

--- Monocritere.m
+++ Monocritere.m
@@ -9,4 +9,21 @@ fResponsableStocks = [1; 1; 1; 1; 1; 1];
 lb = [0 0 0 0 0 0];
 ub = [9999 9999 9999 9999 9999 9999];
 
-[x, benefMax] = linprog(fComptable, cont, b, [], [], lb, ub)
+[x, benefMax] = linprog(fComptable, cont, b, [], [], lb, ub);
+[x, prodMax] = linprog(fResponsableAtelier, cont, b, [], [], lb, ub);
+
+histoStock = zeros(100,1);
+histoProd = zeros(100,1);
+
+for k = 1:100
+    bStock = [4800 4800 4800 4800 4800 4800 4800 850 920 585 k*benefMax/100];
+    bProd = [4800 4800 4800 4800 4800 4800 4800 850 920 585 k*prodMax/100];
+    
+    [xStock, stock] = linprog(fResponsableStocks, contBenef, bStock, [], [], lb, ub);
+    [xProd, prod] = linprog(fResponsableStocks, contProd, bProd, [], [], lb, ub);
+    
+    histoStock(k, 1) = stock;
+    histoProd(k, 1) = prod;
+end
+
+plot(histoStock)
