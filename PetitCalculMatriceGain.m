58146d299630be95767ccd60933d9c60798a7400
Dorian <dorian.lefeuvre@orange.fr>
2018-02-27 14:12:45 +0100

    ajout point

--- PetitCalculMatriceGain.m
+++ PetitCalculMatriceGain.m
@@ -44,6 +44,21 @@ bBenefPers2 = [4800 4800 4800 4800 4800 4800 4800 850 920 585 95*-10004.95/100];
 bBenefPers3 = [4800 4800 4800 4800 4800 4800 4800 850 920 585 90*-10004.95/100];
 [xPers3, benef] = linprog(fResPerso, contBenef, bBenefPers3, [], [], lb, ub, [], options);
 %
+ftemp = transpose(fResPerso);
+contCompta2 = [cont; ftemp];
+bCompta2 = [b 1.30*3890.120];
+[xCompta2, benef] = linprog(fComptable, contCompta2, bCompta2, [], [], lb, ub, [], options);
+%
+ftemp = transpose(fResponsableStocks);
+contCompta3 = [contCompta2; ftemp];
+bCompta3 = [bCompta2 1.25*2039.24];
+[xCompta3, benef] = linprog(fComptable, contCompta3, bCompta3, [], [], lb, ub, [], options);
+%
+ftemp = transpose(fResponsableStocks);
+contCompta4 = [cont; ftemp];
+bCompta4 = [b 1.25*2039.24];
+[xCompta4, benef] = linprog(fComptable, contCompta4, bCompta4, [], [], lb, ub, [], options);
+%
 % pasage de colonne à ligne
 xCom2 = transpose (xCom2);
 xCom3 = transpose (xCom3);
@@ -52,9 +67,12 @@ xStock3 = transpose (xStock3);
 xStock90 = transpose (xStock90);
 xPers2 = transpose (xPers2);
 xPers3 = transpose (xPers3);
+xCompta2 = transpose (xCompta2);
+xCompta3 = transpose (xCompta3);
+xCompta4 = transpose (xCompta4);
 
 % le calcul du gain
-xTousPoints = [ xCompta; xAtelier; xStock; xPersonnel; xCom; xCom2; xCom3; xStock2; xStock3; xStock90; xPers2; xPers3];
+xTousPoints = [ xCompta; xAtelier; xStock; xPersonnel; xCom; xCom2; xCom3; xStock2; xStock3; xStock90; xPers2; xPers3; xCompta2; xCompta3; xCompta4];
 fCritere = [ -fComptable -fResponsableAtelier fResponsableStocks fResPerso fCom];
 gain = xTousPoints * fCritere;
 gainPercent = gain;
